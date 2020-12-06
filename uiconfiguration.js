03a5e178178b36c7bceb8a5f1d343f1bf5a9cebd
abs <abs@ggl.pt>
2020-12-06 00:08:31 +0000

    tunning css

--- uiconfiguration.js
+++ uiconfiguration.js
@@ -7,7 +7,8 @@ var uiConfig = {
 		//firebase.auth.GithubAuthProvider.PROVIDER_ID,
 		{
 			provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-			signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+			signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+			
 		},
 		{
 			provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
