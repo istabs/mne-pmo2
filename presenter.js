a97cd252e08a244a5285e1d5dda78361394551fc
abs <abs@ggl.pt>
2020-12-16 22:06:34 +0000

    adapters tunning - % complete

--- presenter.js
+++ presenter.js
@@ -35,6 +35,7 @@ function googleChartAirtableAdapt(airtableData, airTableAdapter, viewer) {
 		table.addColumn('string', 'Dependencies');
 		var i = 0;
 		items.forEach(item => {
+			console.log(item)
 			table.addRow(item);
 			ids[i++] = item[0];
 		})
